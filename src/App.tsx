5fff09ac934238db3634e76a68ee7066638c8311
FreakyHarsh <harsh.boricha2015@gmail.com>
2021-07-25 13:32:56 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { Button, TextField } from "@material-ui/core";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import classNames from "classnames";
 
 function App() {
   const defaultNoOfMafia = 2;
@@ -21,13 +23,13 @@ function App() {
     const roleTempArr: string[] = [];
 
     for (let i = 0; i < noOfMafia; i++) {
-      roleTempArr[i] = newArr[i] + " mafia";
+      roleTempArr[i] = newArr[i] + " is a MAFIA ☠️";
     }
     for (let j = 0; j < noOfAngel; j++) {
-      roleTempArr[noOfMafia + j] = newArr[noOfMafia + j] + " angel";
+      roleTempArr[noOfMafia + j] = newArr[noOfMafia + j] + " is an ANGEL 😇";
     }
     for (let k = noOfAngel + noOfMafia; k < players.length; k++) {
-      roleTempArr[k] = newArr[k] + " villager";
+      roleTempArr[k] = newArr[k] + " is a villager";
     }
     setRoles(roleTempArr);
   }
@@ -35,13 +37,17 @@ function App() {
   return (
     <div className="App">
       <div>
-        <TextField label="Mafias" variant="outlined" defaultValue="2" onChange={e => setNoOfMafia(+e.target.value)} />
-        <TextField label="Angel" variant="outlined" defaultValue="1" onChange={e => setNoOfAngel(+e.target.value)} />
+        <div className="textfield">
+          <TextField label="Mafias" variant="outlined" defaultValue="2" onChange={e => setNoOfMafia(+e.target.value)} />
+        </div>
+        <div className="textfield">
+          <TextField label="Angel" variant="outlined" defaultValue="1" onChange={e => setNoOfAngel(+e.target.value)} className="textfield" />
+        </div>
       </div>
-      <div>
+      <div className="textfield" id="playerInput">
         <TextField
-          label="Players"
-          variant="standard"
+          label="Player Name"
+          variant="outlined"
           value={currentPlayer}
           onChange={e => setCurrentPlayer(e.target.value)}
           onKeyDown={e => {
@@ -63,29 +69,37 @@ function App() {
           />
         </IconButton>
       </div>
+      <br />
       <div>
         {players.map((player, index) => (
-          <div
-            style={{
-              cursor: "pointer"
-            }}
-            onClick={() => {
-              const newList = [...players];
-              newList.splice(index, 1);
-              setPlayers(newList);
-            }}>
-            {player}
+          <div style={{}} className="playerList">
+            <div>{player}</div>
+            <div>
+              <CloseIcon
+                style={{ color: "red" }}
+                onClick={() => {
+                  const newList = [...players];
+                  newList.splice(index, 1);
+                  setPlayers(newList);
+                }}
+              />
+            </div>
           </div>
         ))}
       </div>
+      <br />
       <div>
         <Button variant="contained" color="primary" onClick={assignRole}>
-          Submit
+          Assign
         </Button>
       </div>
+      <br />
       <div>
         {roles?.map((role, index) => {
-          return <div style={{ padding: "1rem", color: index < noOfMafia ? "red" : index < noOfMafia + noOfAngel ? "green" : "black" }}>{role}</div>;
+          return (
+            <div className={classNames({ mafia: index < noOfMafia, angel: index < noOfAngel + noOfMafia && index >= noOfMafia }, "roleBox")}>{role}</div>
+            // <div className={classNames({ color: index < noOfMafia ? "mafia" : index < noOfMafia + noOfAngel ? "angel" : "villager" }, "roleBox")}>{role}</div>
+          );
         })}
       </div>
     </div>
